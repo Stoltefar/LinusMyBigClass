d9ab830ee7a14586370c85a10b96a7c583210fb8
Linus Callheim <stoltefar@gmail.com>
2022-02-02 13:05:20 +0100

    First commit with constructors

---
+++ src/BigClass.java
@@ -0,0 +1,24 @@
+public class BigClass {
+
+
+    private int number;
+    private String text;
+
+    public BigClass() {
+        this(0, null);
+    }
+
+    public BigClass(int number) {
+        this(number, null);
+    }
+
+    public BigClass(String text) {
+        this(0, text);
+    }
+
+    public BigClass(int number, String text) {
+        this.number = number;
+        this.text = text;
+    }
+
+}
